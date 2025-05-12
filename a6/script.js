7b4fb0f7fddf723e13dae1392c133c71c15d157a
23R11A67A7 <23r11a67a7@gcet.edu.in>
2025-05-12 22:41:52 +0530

    updated

--- a6/script.js
+++ a6/script.js
@@ -1,7 +1,9 @@
-function greet() {
+  function greet() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  const phone=document.getElementById("phone").value;
 
-  const output = `Email: ${email}<br>Password: ${password}`;
+const output = `Email: ${email}<br>Password: ${password}`;
+  const output = `Email: ${email}<br>Password: ${password}<br>Phone no.: ${phone}`;
   document.getElementById("output").innerHTML = output;
 }
